03cde118e743665fc610f847d9ab27061af2276e
Samriddhi Sharma <ssamriddhi2001@gmail.com>
2025-07-21 18:50:29 +0530

    files added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 67328
+let accountEmail = "sam@google.com"
+var accountPassword = "1245352"
+
+accountCity = "Jaipur"
+
+// accountId = 2  // not allowed
+
+accountEmail = "hfjk@ks.com"
+accountPassword = "748922"
+accountCity = "jodhpur"
+let accountState;
+
+console.log(accountId);
+
+/* 
+Prefer not to to use var
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs, not browser
+
+console.log(3+3);
+console.log("hi");
+
+
+let name = "sam"
+let age = 23
+let isloggedIn = false
+
+
