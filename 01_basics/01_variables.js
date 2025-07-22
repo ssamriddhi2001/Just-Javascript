4fa016b111d42da1ea85ecefe8fcd8bb40b87fef
Samriddhi Sharma <ssamriddhi2001@gmail.com>
2025-07-22 11:20:48 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,16 +6,16 @@ accountCity = "Jaipur"
 
 // accountId = 2  // not allowed
 
-accountEmail = "hfjk@ks.com"
-accountPassword = "748922"
+// accountEmail = "hfjk@ks.com"
+// accountPassword = "748922" // commented kyonki gitGuardian se mail aa gaya tha 
 accountCity = "jodhpur"
 let accountState;
 
-console.log(accountId);
+console.log(accountId); 
 
 /* 
 Prefer not to to use var
 because of issue in block scope and functional scope 
 */
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -10,4 +10,18 @@ let name = "sam"
 let age = 23
 let isloggedIn = false
 
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true
+//null => standalone value
+//undefined => value not assigned yet
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
+
 
