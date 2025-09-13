fd0c658c254db22e7d1bc7b94fcd101cecb745b6
Samriddhi Sharma <ssamriddhi2001@gmail.com>
2025-09-13 16:37:46 +0530

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+// const month = 3
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     default:
+//         console.log("default case match");
+        
+//         break;
+// }
