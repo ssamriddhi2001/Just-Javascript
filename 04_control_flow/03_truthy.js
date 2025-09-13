305e8fe231c2a81a14a202f58c3c3726e885987d
Samriddhi Sharma <ssamriddhi2001@gmail.com>
2025-09-13 17:19:10 +0530

    truthy & falsy

---
+++ 04_control_flow/03_truthy.js
@@ -0,0 +1,44 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("Got user email");
+    
+// } else {
+//     console.log("Don't have user email");
+    
+// }
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");   
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+    
+}
+
+// Nullish Coalescing Operator (??): null undefined ~~~ used for a flag value
+
+let val1;
+// val1 - 5 ?? 10
+// val1 = null ?? 10 ~~~~// 10
+// val1 = undefined ?? 15 ~~~ // 15
+// val1 = null ?? 10 ?? 20 ~~~// 10
+val1 =  "sam" ?? "Ashu" //~~~ sam
+
+console.log(val1);
+
+// terniary operator
+
+// condition ? true : false 
+
+const iceTeaprice = 100
+iceTeaprice <= 80 ? console.log("less than 80") : console.log("more than 80");
