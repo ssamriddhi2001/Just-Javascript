63ed79d4fb27244eab77ac92090cab8c900e7a53
Samriddhi Sharma <ssamriddhi2001@gmail.com>
2025-09-23 17:08:28 +0530

    for(break & continue)

---
+++ 05_iterations/one.js
@@ -0,0 +1,51 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("5 is the best number");
+        
+    }
+    // console.log(element);
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value:${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`iNner loop value:${j} and inner loop ${i}`);
+
+        // console.log(`${i} * ${j} = ${(i*j)}`);
+        
+        
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+// console.log(myArray.length);
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+//break and continue
+
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+        //  console.log("Detected 5");
+         break;
+    } // break is used to break through or to stop after specific condition
+    // console.log(`Value of i is ${index}`);
+}
+
+for (let index = 1; index < 20; index++) {
+    if (index == 5) {
+         console.log("Detected 5");
+         continue; // continue is skipped for once
+    } 
+    console.log(`Value of i is ${index}`);
+}
